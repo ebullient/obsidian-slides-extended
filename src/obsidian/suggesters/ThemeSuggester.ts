50194b1f78306ee6900fd2bd6c62b518a3a9ebce
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-03-20 10:14:55 -0400

    fix: Switch to Obsidian provided popover suggester (via obsidian-utilities module)

--- src/obsidian/suggesters/ThemeSuggester.ts
+++ src/obsidian/suggesters/ThemeSuggester.ts
@@ -1,78 +1,60 @@
-// Credits go to Liam's Periodic Notes Plugin: https://github.com/liamcain/obsidian-periodic-notes
-import { App } from 'obsidian';
+import { FuzzyMatch } from 'obsidian';
 import fs from 'fs';
-import { TextInputSuggest } from './Suggest';
 import { ObsidianUtils } from '../obsidianUtils';
+import { FuzzyInputSuggest } from 'obsidian-utilities';
 
 const highlightCss = (basename: string): boolean => {
     return basename.contains('highlight') || basename.contains('hljs');
 };
 const themeCss = (basename: string) => !highlightCss(basename);
 
-export class ThemeSuggest extends TextInputSuggest<string> {
-    readonly utils: ObsidianUtils;
-    readonly themeFiles: string[];
-    constructor(
-        type: string,
-        app: App,
-        inputEl: HTMLInputElement | HTMLTextAreaElement,
-        utils: ObsidianUtils,
-    ) {
-        super(app, inputEl);
-        this.utils = utils;
+const getFiles = (
+    directories: string[],
+    include: (dir: string, basename: string) => boolean,
+) => {
+    const result = new Set<string>();
 
-        const searchPath =
-            type == 'highlight'
-                ? this.utils.getHighlightSearchPath()
-                : this.utils.getThemeSearchPath();
-
-        // ==> hljs or highlight
-        this.themeFiles = Array.of(
-            ...this.getFiles(searchPath, (_: string, basename: string) => {
-                if (basename.endsWith('.css')) {
-                    return type == 'highlight'
-                        ? highlightCss(basename)
-                        : themeCss(basename);
-                }
-                return false;
-            }),
-        );
-    }
-
-    getSuggestions(inputStr: string): string[] {
-        const files: string[] = [];
-        const lowerCaseInputStr = inputStr.toLowerCase();
-        this.themeFiles.forEach((file: string) => {
-            if (file.toLowerCase().includes(lowerCaseInputStr)) {
-                files.push(file);
+    for (const directory of directories) {
+        fs.readdirSync(directory).forEach(file => {
+            if (include(directory, file)) {
+                result.add(file);
             }
         });
-        return files;
     }
+    return result;
+};
 
-    getFiles(
-        directories: string[],
-        include: (dir: string, basename: string) => boolean,
-    ) {
-        const result = new Set<string>();
+export const getThemeFiles = (
+    utils: ObsidianUtils,
+    type: 'highlight' | 'theme',
+): string[] => {
+    const searchPath =
+        type == 'highlight'
+            ? utils.getHighlightSearchPath()
+            : utils.getThemeSearchPath();
+    console.log('🚀 ~ file: ThemeSuggester.ts:32 ~ searchPath:', searchPath);
 
-        for (const directory of directories) {
-            fs.readdirSync(directory).forEach(file => {
-                if (include(directory, file)) {
-                    result.add(file);
-                }
-            });
-        }
-        return result;
+    // ==> hljs or highlight
+    return [
+        ...getFiles(searchPath, (_: string, basename: string) => {
+            if (basename.endsWith('.css')) {
+                return type == 'highlight'
+                    ? highlightCss(basename)
+                    : themeCss(basename);
+            }
+            return false;
+        }),
+    ];
+};
+
+export class ThemeInputSuggest extends FuzzyInputSuggest<string> {
+    getItemText(item: string): string {
+        return item;
     }
-
-    renderSuggestion(file: string, el: HTMLElement): void {
-        el.setText(file);
+    renderNote(noteEL: HTMLElement, result: FuzzyMatch<string>): void {
+        //no-op
     }
-
-    selectSuggestion(file: string): void {
-        this.inputEl.value = file;
-        this.inputEl.trigger('input');
-        this.close();
+    renderTitle(titleEl: HTMLElement, result: FuzzyMatch<string>): void {
+        this.renderMatches(titleEl, result.item, result.match.matches);
     }
 }
