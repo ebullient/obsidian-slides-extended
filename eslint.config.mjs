1b4e8666ea2e3f6413e141b36fb7fc7126912565
Erin Schnabel <erinschnabel@gmail.com>
2024-10-07 12:36:42 -0400

    🩹 cleanup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/node_modules", "**/build", "**/test-vault", "**/examples", "**/docs"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-unused-vars": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            args: "none",
+        }],
+
+        "@typescript-eslint/ban-ts-comment": "off",
+        "no-prototype-builtins": "off",
+        "@typescript-eslint/no-empty-function": "off",
+    },
+}];
