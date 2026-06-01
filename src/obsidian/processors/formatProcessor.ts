d75365cd941f6fd6d64240cd6b30a4c93792f632
Peter Yanase <54794619+peter-yanase@users.noreply.github.com>
2026-06-01 09:44:56 +0900

    regex fix

--- src/obsidian/processors/formatProcessor.ts
+++ src/obsidian/processors/formatProcessor.ts
@@ -1,24 +1,31 @@
+// Process formatting
+// See docs/content/basic-syntax/textStyle.md
+
 import type { Options, Processor } from "../../@types";
 import { processBySlide, skipMathCodeBlocks } from "../obsidianUtils";
 
 export class FormatProcessor implements Processor {
-    private markRegex = /==([^=]*)==/gm;
-    private commentRegex = /%%([^%]*)%%/gm;
+    private highlightedRegex = /==([\s\S]*?)==/g;
+    private commentRegex = /%%([\s\S]*?)%%/g;
 
-    process(markdown: string, options: Options) {
+    process(markdown: string, options?: Options) {
         let output = markdown;
-        processBySlide(markdown, options, (slide) => {
-            let newSlide = slide;
-            newSlide = skipMathCodeBlocks(slide, (md) => this.formatText(md));
-            output = output.replace(slide, () => newSlide);
-            return newSlide;
-        });
+        if (options) {
+            processBySlide(markdown, options, (slide) => {
+                let newSlide = slide;
+                newSlide = skipMathCodeBlocks(slide, (md) =>
+                    this.formatText(md),
+                );
+                output = output.replace(slide, () => newSlide);
+                return newSlide;
+            });
+        }
         return output;
     }
 
     private formatText(markdown: string): string {
         return markdown
-            .replaceAll(this.markRegex, "<mark>$1</mark>")
+            .replaceAll(this.highlightedRegex, "<mark>$1</mark>")
             .replaceAll(this.commentRegex, "");
     }
 }
